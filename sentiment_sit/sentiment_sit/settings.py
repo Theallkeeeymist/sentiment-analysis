6977bccc7bd9caa5aba1660373420e8b4f45af36
Sudhanshu Anand <sudhanshu.anand1143@gmail.com>
2025-06-18 21:21:56 +0530

    Updates

--- sentiment_sit/sentiment_sit/settings.py
+++ sentiment_sit/sentiment_sit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=%3ttly89k$%se_8f_*-eb)m4&6e^vq%ld(us=wot8a@v$a_s5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
