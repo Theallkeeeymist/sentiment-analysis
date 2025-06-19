5a41a99886219d4c75b0e0a43ef1ef199ca5cf3f
Sudhanshu Anand <sudhanshu.anand1143@gmail.com>
2025-06-19 13:54:29 +0530

    personal details

--- sentiment_sit/analyzer/nltk_downloader.py
+++ sentiment_sit/analyzer/nltk_downloader.py
@@ -4,3 +4,6 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet') #to use wordnetlemmetizer
 nltk.download('omw-1.4')
+
+# Start Command
+# cd sentiment_sit && pip install -r .../requirements.txt && python analyzer/nltk_downloader.py && python manage.py collectstatic --noinput && gunicorn sentiment_sit.wsgi:application
